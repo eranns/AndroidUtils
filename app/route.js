450df4402169e8c947eba694fc3044f198fd2fc4
eransar@post.bgu.ac.il <eransar@post.bgu.ac.il>
2019-08-15 17:00:19 +0300

    fixed routing

---
+++ app/route.js
@@ -0,0 +1,32 @@
+var app = angular.module('mainApp', ["ngRoute"]);
+app.config(function($routeProvider)  {
+    "use strict";
+    $routeProvider
+    // homepage
+        .when('/', {
+            templateUrl: "./home/home.html",
+            controller : 'homeController as homeCtrl',
+            css: 'home/home.css'
+        })
+        .when('/home', {
+            templateUrl: 'home/home.html',
+            controller : 'homeController as homeCtrl',
+            css: 'home/home.css'
+        })
+        .when('/device', {
+            templateUrl: './device/device.html',
+            controller : 'deviceController as deviceCtrl',
+            css: 'device/device.css',
+        })
+        .when('/apk', {
+            templateUrl: 'apk/apk.html',
+            controller : 'apkController as apkCtrl',
+            css: 'apk/apk.css'
+        })
+        .when('/about', {
+            templateUrl: 'about/about.html',
+            controller : 'aboutController as abtCtrl',
+            css: 'about/about.css'
+        })
+        .otherwise({ redirectTo: '/' });
+});
