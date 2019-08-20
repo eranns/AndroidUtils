8db75b11e1ab3357f66e15c4edff43f016b4cbc3
eransar@post.bgu.ac.il <eransar@post.bgu.ac.il>
2019-08-20 17:35:24 +0300

    simple card design.

--- app/home/home.js
+++ app/home/home.js
@@ -1,6 +1,9 @@
 const {remote} = require('electron');
 const adb=remote.require('./sys/adb/adb');
 var app=angular.module("mainApp").controller("homeController", function (deviceService, $scope) {
+    //
+    $scope.devices="";
+
     $scope.devices =  deviceService.getAvilableDevices().then(function (result) {
         $scope.devices = result;
         $scope.$digest()
@@ -8,22 +11,50 @@ var app=angular.module("mainApp").controller("homeController", function (deviceS
         console.log(error);
     });
 
+    $scope.isAvailable=function(){
+        return deviceService.isConnected();
+    }
+
+    $scope.deviceClick=function(event){
+        console.log(event)
+        deviceService.setcurrentDevice(event.target.id);
+
+        console.log(deviceService.getcurrentDevice())
+    }
+
+
 });
 
 
 
+
+
 app.service('deviceService', function () {
+    var currentdevice= "";
+    var numberOfDevices=0;
     // console.log(await favourites.getDevices());
 
     this.getAvilableDevices = async function () {
         try{
             // console.log(await adb.getDevices());
-            return await adb.getDevices();
+            var result = await adb.getDevices();
+            numberOfDevices=result.length;
+            return result;
         }
         catch (e) {
             console.log("getAvilableDevices error "+e)
         }
 
+    };
+    this.getcurrentDevice = function (){
+        return currentdevice;
+    };
+
+    this.setcurrentDevice = function(device){
+        currentdevice=device;
+    };
+    this.isConnected=function(){
+        return numberOfDevices >0;
     }
 
     this.getProperties = async function(devices){
